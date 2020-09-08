ea0e45aa9730dff65b90697d8df529492915679e
lingfeng23 <15131257162@163.com>
2020-09-09 06:18:27 +0800

    增加 Security 部分

---
+++ springboot/src/main/java/malf/entity/Reader.java
@@ -0,0 +1,81 @@
+package malf.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * @author malf
+ * @description 定义 Reader 的JPA实体
+ * @project bookStudy
+ * @since 2020/9/9
+ */
+@Entity
+public class Reader implements UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String username;
+	private String fullname;
+	private String password;
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * 授予 READER 权限
+	 * @return
+	 */
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("READER"));
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
