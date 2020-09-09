16b2357f865920ea9e38652a039c57fa96f2c268
lingfeng23 <15131257162@163.com>
2020-09-10 07:39:48 +0800

    配置log4j查看mybatis操作数据库的过程

---
+++ mybatis/src/test/java/malf/mapper/CountryMapperTest.java
@@ -0,0 +1,51 @@
+package malf.mapper;
+
+import malf.model.Country;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+/**
+ * @author malf
+ * @description TODO
+ * @project bookStudy
+ * @since 2020/9/9
+ */
+public class CountryMapperTest {
+	private static SqlSessionFactory sqlSessionFactory;
+
+	@BeforeClass
+	public static void init() {
+		try {
+			Reader reader  = Resources.getResourceAsReader("mybatis-config.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			reader.close();
+		} catch (IOException e) {
+
+		}
+	}
+
+	@Test
+	public void testSelectAll() {
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		try {
+			List<Country> countries = sqlSession.selectList("selectAll");
+			print(countries);
+		} finally {
+			sqlSession.close();
+		}
+	}
+	private void print(List<Country> countries) {
+		for (Country country: countries) {
+			System.out.printf("%-4d%4s%4s\n",
+					country.getId(), country.getCountryname(), country.getCountrycode());
+		}
+	}
+}
