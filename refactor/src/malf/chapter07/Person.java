e76423455f56e67f06fba6927a74979efbfbb2a5
malf <15131257162@163.com>
2020-09-16 19:42:08 +0800

    重构

---
+++ refactor/src/malf/chapter07/Person.java
@@ -0,0 +1,14 @@
+package malf.chapter07;
+
+/**
+ * @author malf
+ * @createTime 2020/9/16
+ * @description 类描述
+ */
+public class Person {
+	private String name;
+	// 下面两个字段提炼为心类 Phone
+//	private String phoneAreaCode;
+//	private String phoneNumber;
+	private Phone phone;
+}
---
+++ refactor/src/malf/chapter07/Phone.java
@@ -0,0 +1,11 @@
+package malf.chapter07;
+
+/**
+ * @author malf
+ * @createTime 2020/9/16
+ * @description 类描述
+ */
+public class Phone {
+	private String areaCode;
+	private String number;
+}
